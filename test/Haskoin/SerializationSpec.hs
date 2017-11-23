504cda4bb69e04f7d392e6bc287bd43f11d4c957
Lorenzo Lopez Sancho <lorenzo.lopez@intec.es>
2017-11-23 07:42:39 +0100

    Add serialization

---
+++ test/Haskoin/SerializationSpec.hs
@@ -0,0 +1,21 @@
+module Haskoin.SerializationSpec (main, spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Haskoin.Mining
+import Haskoin.Serialization
+import Protolude
+import qualified Data.ByteString.Base16.Lazy as BSL
+
+-- `main` is here so that this module can be run from GHCi on its own.  It is
+-- not needed for automatic spec discovery.
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Genesis" $ do
+    it "Serializes Genesis" $ do
+      genesis <- makeGenesis
+      BSL.encode(serialize genesis) `shouldBe` "000000000000000000"
