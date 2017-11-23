1fe2df75c2f64b38238d355c92188844ae4724c3
Lorenzo Lopez Sancho <lorenzo.lopez@intec.es>
2017-11-23 09:42:41 +0100

    Add basic mining

---
+++ src/Haskoin/Mining.hs
@@ -0,0 +1,21 @@
+module Haskoin.Mining where
+
+import Haskoin.Types
+import Haskoin.Serialization
+import Crypto.Hash(hashlazy)
+
+type TransactionPool = IO [Transaction]
+
+mineOn :: TransactionPool -> Account -> Blockchain -> IO Blockchain
+mineOn pendingTransactions minerAccount parent = do
+  ts <- pendingTransactions
+  let block = Block ts
+  let header = BlockHeader {
+      _minerAccount = minerAccount,
+      _parentHash = hashlazy $ serialize parent
+      }
+  return $ Node block header parent
+
+
+makeGenesis :: IO Blockchain
+makeGenesis = return $ Genesis (Block [])
