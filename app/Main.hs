6fd3866ff8f3a85040e8ff6fbe8b543f141bc4a8
Lorenzo Lopez Sancho <lorenzo.lopez@intec.es>
2017-11-23 09:49:36 +0100

    Update main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Haskoin.Types
+import Haskoin.Mining
 import Text.Pretty.Simple
 import Crypto.Hash(hashlazy)
 import Data.ByteString.Lazy.Char8(pack)
@@ -10,17 +11,12 @@ getHash value = hashlazy (pack value) :: HaskoinHash
 
 main :: IO ()
 main = do
-  let genesisBlock = Genesis (Block [])
-      firstBlock   = Node (Block [Transaction 0 1 1000]) 
-                        BlockHeader { _minerAccount = 0, _parentHash = getHash "genesisHash" } 
-                        genesisBlock
-      chain        = Node (Block [Transaction 1 0 1000]) 
-                        BlockHeader { _minerAccount = 0, _parentHash = getHash "firstHash" } 
-                        firstBlock
-      txns         = mconcat $ map transactions (blocks chain)
+  genesis <- makeGenesis
+  chain <- mineOn (return [Transaction 0 1 1000]) 2 genesis
+  chain <- mineOn (return [Transaction 1 0 1000]) 2 chain
 
   pPrint chain
-  pPrint txns
+  pPrint $ mconcat $ map transactions (blocks chain)
 
 
 
