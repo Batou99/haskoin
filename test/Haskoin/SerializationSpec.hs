ac2409106e7507371aad10d1edd4079f44b635c0
Lorenzo Lopez Sancho <lorenzo.lopez@intec.es>
2017-11-23 08:43:58 +0100

    Add missing import

--- test/Haskoin/SerializationSpec.hs
+++ test/Haskoin/SerializationSpec.hs
@@ -4,6 +4,7 @@ import Test.Hspec
 import Test.QuickCheck
 
 import Haskoin.Serialization
+import Haskoin.Types
 import qualified Data.ByteString.Base16.Lazy as BSL
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
